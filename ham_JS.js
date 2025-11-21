6d4879ce729c0df8d63a46d0a6744e2ac904c448
nguyenthanhlongvn03 <nguyenthanhlongvn03@gmail.com>
2025-11-21 09:48:01 +0700

    add

---
+++ ham_JS.js
@@ -0,0 +1,44 @@
+// function showDialog() {
+//     alert('Xin chào các huynh đệ ');
+// }
+// showDialog();
+// function write_log(mana, mana2){
+//     console.log(mana);
+//     console.log(mana2);
+// }
+// write_log('xin chào các huynh đệ', 'abcd');
+/* function wirite_log(){
+    //console.log(arguments);
+    let my_String ='';
+    for (let param of arguments) {
+        //console.log(param);
+        my_String += `${param} - `;
+    }
+    console.log(my_String);
+}
+wirite_log('log1', 'log2', 'log3', 4, 5, 6);
+ */
+// định nghĩa biến trong hàm
+    /*
+    function show(){
+        let name = 'mambo yeah';
+        console.log(name);
+    }
+    show();
+     */
+// định nghĩa hàm trong hàm
+/*
+    function show(){
+        function show_2(){
+            console.log('show_2')
+        }
+        show_2();
+    }
+    show();
+ */
+function show(){
+
+}
+let show_2 = function (){
+
+}
